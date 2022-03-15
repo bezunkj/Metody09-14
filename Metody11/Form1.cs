cf9dd9ddad9deed2a48389515806d97c71b7e4a8
bezunkj.04 <bezunkj.04@A321-10>
2022-03-15 09:00:20 +0100

    Metody11

--- Metody11/Form1.cs
+++ Metody11/Form1.cs
@@ -16,5 +16,32 @@ namespace Metody11
         {
             InitializeComponent();
         }
+
+        private int PocetSlov(string x, out string newString)
+        {
+            char[] separators = { ' ' };
+            string[] slova = x.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            newString = x;
+
+            for (int i = 0; i < newString.Length; i++)
+            {
+                if (char.IsDigit(newString[i]))
+                {
+                    newString = newString.Remove(i, 1);
+                    i--;
+                }
+            }
+
+            return slova.Length;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string x = textBox1.Text;
+            string bezCislic;
+            int pocet = PocetSlov(x, out bezCislic);
+            MessageBox.Show("Pocet slov: " + pocet + "\nRetezec bez cislic: " + bezCislic);
+        }
     }
 }
