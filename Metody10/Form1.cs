0b8e350f3321085fcd1cd97e66fcd1792627638b
bezunkj.04 <bezunkj.04@A321-10>
2022-03-15 08:38:26 +0100

    Metody10

--- Metody10/Form1.cs
+++ Metody10/Form1.cs
@@ -16,5 +16,48 @@ namespace Metody10
         {
             InitializeComponent();
         }
+
+        private bool ObsahujeCislici(string x, out int cifSoucet, out int soucLichCif, out int soucSudCif)
+        {
+            bool obsahujeCifru = false;
+            cifSoucet = 0;
+            soucLichCif = 0;
+            soucSudCif = 0;
+            foreach (char znak in x)
+            {
+                if (char.IsDigit(znak))
+                {
+                    obsahujeCifru = true;
+                    int cislo = Convert.ToInt32(char.ToString(znak));
+                    cifSoucet += cislo;
+                    if(cislo % 2 == 0)
+                    {
+                        soucSudCif += cislo;
+                    } else
+                    {
+                        soucLichCif += cislo;
+                    }
+                    
+                }
+            }
+            return obsahujeCifru;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string x = textBox1.Text;
+            int cifSouc;
+            int soucLich;
+            int soucSud;
+
+            if(ObsahujeCislici(x,out cifSouc, out soucLich, out soucSud))
+            {
+                MessageBox.Show("Retezec obsahuje alespon 1 cislici\ncif. soucet: " + cifSouc + "\nsouc. lich. cifer: " + soucLich + "\nsouc. sud. cifer: " + soucSud);
+            } else
+            {
+                MessageBox.Show("Retezec neobsahuje zadnou cislici");
+            }
+
+        }
     }
 }
